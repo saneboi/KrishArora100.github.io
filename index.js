84a687088f66476c9a711eee087bdbb16cf4a053
KrishArora100 <85039577+KrishArora100@users.noreply.github.com>
2021-05-29 22:08:20 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+  function findTherapist(){
+    var zipCode = document.getElementById("zipCode").value;
+    var zipCodeLink = "https://www.psychologytoday.com/us/therapists?search=" + zipCode;
+    document.write('<a href="' + zipCodeLink + '">Click to find therapists near you</a>');
+    window.location.href=zipCodeLink;
+  }
+
+	function sendEmail() {
+	Email.send({
+		Host: "smtp.gmail.com",
+        Username: "krarora@gmail.com",
+        Password: "Thisistest1",
+        To: 'meenakshi17@gmail.com',
+        From: "krarora100@gmail.com",
+        Subject: "Someone has reached out to you.",
+        Body: "You have recieved a email from someone who would like help.",
+	})
+		.then(function (message) {
+		alert("mail sent successfully")
+		});
+	}
+
