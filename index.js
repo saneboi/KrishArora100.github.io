d0ef0f9e0a04ef0153ac7639f1677c42b4559506
KrishArora100 <85039577+KrishArora100@users.noreply.github.com>
2021-05-29 22:21:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ Email.send({
   Host: "smtp.gmail.com",
   Username: "krarora@gmail.com",
   Password: "Thisistest1",
-  To: 'meenakshi17@gmail.com',
-  From: "krarora100@gmail.com",
-  Subject: "Someone has reached out to you.",
-  Body: "You have recieved a email from someone who would like help.",
-})
-Email.send({
-  Host: "smtp.gmail.com",
-  Username: "krarora@gmail.com",
-  Password: "Thisistest1",
-  To: 'meenakshi17@gmail.com',
+  To: 'meenakshi17@gmail.com, meenakshi17@yahoo.com',
   From: "krarora100@gmail.com",
   Subject: "Someone has reached out to you.",
   Body: "You have recieved a email from someone who would like help.",
